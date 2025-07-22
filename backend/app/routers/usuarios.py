0612c62bf63d3493019f5a34d917c1a554e06d6d
sebaslade <sebaslade29@gmail.com>
2025-07-22 17:32:09 -0500

    feat: Registrar usuarios

--- backend/app/routers/usuarios.py
+++ backend/app/routers/usuarios.py
@@ -4,6 +4,7 @@ from app.core.database import get_db
 from app.models import Usuario
 from app.schemas import UsuarioBase, UsuarioCreate, UsuarioResponse
 from passlib.context import CryptContext
+from typing import List
 
 router = APIRouter()
 
@@ -11,6 +12,18 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
+
+@router.get("/usuarios/", response_model=List[UsuarioResponse])
+def listar_usuarios(db: Session = Depends(get_db)):
+    usuarios = db.query(Usuario).all()
+    return usuarios
+
+@router.get("/usuarios/{usuario_id}", response_model=UsuarioResponse)
+def obtener_usuario(usuario_id: int, db: Session = Depends(get_db)):
+    usuario = db.query(Usuario).filter(Usuario.id == usuario_id).first()
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    return usuario
 
 @router.post("/usuarios/", response_model=UsuarioResponse, status_code=status.HTTP_201_CREATED)
 def crear_usuario(usuario: UsuarioCreate, db: Session = Depends(get_db)):
@@ -30,10 +43,3 @@ def crear_usuario(usuario: UsuarioCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(nuevo_usuario)
     return nuevo_usuario
-
-@router.get("/usuarios/{usuario_id}", response_model=UsuarioResponse)
-def obtener_usuario(usuario_id: int, db: Session = Depends(get_db)):
-    usuario = db.query(Usuario).filter(Usuario.id == usuario_id).first()
-    if not usuario:
-        raise HTTPException(status_code=404, detail="Usuario no encontrado")
-    return usuario
